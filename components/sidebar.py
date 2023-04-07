6c7de1903eff7237db7c984054ba8cc96c607311
João Barros Malho <113609805+joaomalho@users.noreply.github.com>
2023-04-07 21:51:43 +0100

    Update sidebar.py

--- components/sidebar.py
+++ components/sidebar.py
@@ -3,13 +3,11 @@ import dash
 from dash import html, dcc
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
-from app import app
 
 from datetime import datetime, date
 import plotly.express as px
 import numpy as np
 import pandas as pd
-
 
 import pdb
 from dash_bootstrap_templates import ThemeChangerAIO
