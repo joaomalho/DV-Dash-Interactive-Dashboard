e92b55be9e0ae056105ae9b91ec928348d2f5e9e
João Barros Malho <113609805+joaomalho@users.noreply.github.com>
2023-04-07 21:52:12 +0100

    Update wine.py

--- components/wine.py
+++ components/wine.py
@@ -21,8 +21,6 @@ from dash.dash_table.Format import Group
 from dash.dependencies import Input, Output
 
 from data import *
-from app import app
-
 
 
 # This dictionary defines the parameters of each icon in kpi
